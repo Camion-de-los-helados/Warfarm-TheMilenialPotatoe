c501e049831d5dc2834254713bf007698a6d19b3
Daniel Carrasco <dacarr05@ucm.es>
2023-02-05 11:54:13 +0100

    delete coherence

--- FarmWars/Assets/Scripts/ContClicks.cs
+++ FarmWars/Assets/Scripts/ContClicks.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+//using Win;
 
 public class ContClicks : MonoBehaviour, IPointerDownHandler
 {
-    UnityEvent WinEvent; 
+    
     int MaxClick = Const.MAX_CLICKS_POTATOPEELER;
-    public int  ActualClick = 0;
-    [SerializeField] private Button btn;
+    public int  ActualClick1 = 0;
+    public int  ActualClick2 = 0;
+    //[SerializeField] private Button btn;
     [SerializeField] private TextMeshProUGUI text;
+    //[SerializeField] private Win win;
     public Sprite p1;
     public Sprite p2;
     public Sprite p3;
@@ -22,13 +25,18 @@ public class ContClicks : MonoBehaviour, IPointerDownHandler
     public Sprite ButtonView;
     public Sprite ButtonViewPressed;
     public Image imageChange;
+    public Image imageChange2;
     public Button ButtonImage;
     // Start is called before the first frame update
 
+    
+
     private void Start() 
     {
-        btn.onClick.AddListener(()=>Counter());
-        ButtonImage.image.sprite = ButtonView;
+        
+        
+        //btn.OnKeyDown.AddListener(()=>Counter());
+        //ButtonImage.image.sprite = ButtonView;
     }
 
     // Update is called once per frame
@@ -38,39 +46,96 @@ public class ContClicks : MonoBehaviour, IPointerDownHandler
     {
         ButtonImage.image.sprite = ButtonViewPressed;
     }
-    public void Counter()
+    public void Counter1()
     {
-        ButtonImage.image.sprite = ButtonView;
-        ActualClick++;
+        //ButtonImage.image.sprite = ButtonView;
+        ActualClick1++;
         //text.text = ActualClick.ToString();
         float range= MaxClick/5;
 
-        if (ActualClick == range)
+        if (ActualClick1 == range)
         {
             imageChange.sprite = p2;
-        }else if(ActualClick == range*2)
+        }else if(ActualClick1 == range*2)
         {
             imageChange.sprite = p3;
         }
-        else if(ActualClick == range*3)
+        else if(ActualClick1 == range*3)
         {
             imageChange.sprite = p4;
         }
-        else if(ActualClick == range*4)
+        else if(ActualClick1 == range*4)
         {
             imageChange.sprite = p5;
         }
-        else if(ActualClick == MaxClick)
+        else if(ActualClick1 == MaxClick)
         {
             imageChange.sprite = p6;
-            WinEvent.Invoke();
+           // win.FinishGame(0);
+            EndGame();
         }
         
+    }
+
+
+
+
+    public void Counter2()
+    {
+        //ButtonImage.image.sprite = ButtonView;
+        ActualClick2++;
+        //text.text = ActualClick.ToString();
+        float range = MaxClick / 5;
+
+        if (ActualClick2 == range)
+        {
+            imageChange2.sprite = p2;
+        }
+        else if (ActualClick2 == range * 2)
+        {
+            imageChange2.sprite = p3;
+        }
+        else if (ActualClick2 == range * 3)
+        {
+            imageChange2.sprite = p4;
+        }
+        else if (ActualClick2 == range * 4)
+        {
+            imageChange2.sprite = p5;
+        }
+        else if (ActualClick2 == MaxClick)
+        {
+            imageChange.sprite = p6;
+          //  win.FinishGame(1);
+            EndGame();
+        }
+
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         ButtonImage.image.sprite = ButtonViewPressed;
     }
+
     
+
+    public void EndGame()
+    {
+
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+         //   ButtonImage.image.sprite = ButtonViewPressed;
+            Counter1();
+        }else if (Input.GetKeyDown(KeyCode.P))
+        {
+        //    ButtonImage.image.sprite = ButtonViewPressed;
+            Counter2();
+        }
+    }
+
 }
