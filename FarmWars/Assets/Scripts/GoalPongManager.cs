2cf8d44a5e9231f0dd3cb961faabbf36156e8d2e
Gaspar <UO250825@uniovi.es>
2023-02-05 10:22:36 +0100

    Win refactor

--- FarmWars/Assets/Scripts/GoalPongManager.cs
+++ FarmWars/Assets/Scripts/GoalPongManager.cs
@@ -2,32 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using TMPro;
 using UnityEngine.UI;
 
 public class GoalPongManager : MonoBehaviour
 {
+    [SerializeField]
+    int PlayerID0;
 
-    public Canvas canvas;
-    [SerializeField] TextMeshProUGUI tmp;
-    [SerializeField] Image image;
-    [SerializeField] Sprite p1Sprite;
-    [SerializeField] Sprite p2Sprite;
-    [SerializeField] int PlayerID0;
-    [SerializeField] int PlayerID1;
+    [SerializeField]
+    int PlayerID1;
+
+    [SerializeField]
+    Win win;
 
     public void EndGame(int PlayerIDWin)
     {
-        tmp.text = "PLAYER " + (PlayerIDWin + 1) + " WINS";
-        if (PlayerIDWin.Equals(0))
-        {
-            image.sprite = p1Sprite;
-        }
-        else
-        {
-            image.sprite = p2Sprite;
-        }
-        canvas.GetComponent<Canvas>().enabled = true;
+        win.FinishGame (PlayerIDWin);
         Debug.Log("HA GANADO EL PLAYER = " + PlayerIDWin);
     }
 }
