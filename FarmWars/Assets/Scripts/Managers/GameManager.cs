9044f671656e93e8dc4c736778da1b52d7408808
Gaspar <UO250825@uniovi.es>
2023-02-05 05:10:05 +0100

    New Cards

--- FarmWars/Assets/Scripts/Managers/GameManager.cs
+++ FarmWars/Assets/Scripts/Managers/GameManager.cs
@@ -6,9 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    #region References
+#region References
     public static GameManager m_gameManager { get; private set; }
+
     public GridManager m_gridManager { get; private set; }
+
     public CardManager CardManager { get; private set; }
 
     [SerializeField]
@@ -21,38 +23,42 @@ public class GameManager : MonoBehaviour
     private GameObject PotatoBlockPrefab;
 
 
-    #endregion
+#endregion
 
-    #region Properties
-    #region Menu
+
+
+#region Properties
+#region Menu
     [Header("Sound")]
     [Range(0, 100)]
     public float _soundVolume;
+
     [Range(0, 100)]
     public float _effectsVolume;
 
     [Header("MiniGames")]
     public GameObject _lastWinner;
-    #endregion
+#endregion
+
+
     public Player LocalPlayer;
+
     public Player RemotePlayer;
 
     public Player LastMiniGameWinner;
 
     public Scene activeScene;
-    #endregion
+#endregion
 
-    #region Methods
+
+
+#region Methods
     public void LoadScene(int id)
     {
         activeScene = SceneManager.GetSceneAt(id);
-        SceneManager.LoadScene(id);
+        SceneManager.LoadScene (id);
         SceneChanged();
     }
-
-
-
-
 
     private void SceneChanged()
     {
@@ -62,14 +68,15 @@ public class GameManager : MonoBehaviour
                 CardManager.Instance.LoadSceneVariables(true);
 
                 break;
-
             default:
                 Debug.LogWarning("Scene not found");
                 break;
         }
     }
 
-    #endregion
+
+#endregion
+
 
     private void Awake()
     {
@@ -86,12 +93,13 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         DontDestroyOnLoad(this);
-        //m_gridManager = GridManager.Instance;
 
+        //m_gridManager = GridManager.Instance;
         GameObject GO = new GameObject();
         GO.name = "CardManager";
+
         //Instantiate(GO);
-        GO.AddComponent(typeof(CardManager));
+        GO.AddComponent(typeof (CardManager));
 
         if (PotatoBombPrefab == null || PotatoJumpinPrefab == null)
         {
@@ -102,18 +110,16 @@ public class GameManager : MonoBehaviour
         CardManager.Instance.PotatoJumpinPrefab = PotatoJumpinPrefab;
         CardManager.Instance.PotatoBlockPrefab = PotatoBlockPrefab;
 
-        //LocalPlayer = new Player();
-        //RemotePlayer = new Player();
-
+        //LocalPlayer = new Player(0);
+        //RemotePlayer = new Player(1);
         activeScene = SceneManager.GetActiveScene();
 
-        CardManager.Instance.DrawCard(LocalPlayer);
-        CardManager.Instance.DrawCard(LocalPlayer);
-        CardManager.Instance.DrawCard(LocalPlayer);
+        CardManager.Instance.DrawCard (LocalPlayer);
+        CardManager.Instance.DrawCard (LocalPlayer);
+        CardManager.Instance.DrawCard (LocalPlayer);
         SceneChanged();
-
-
     }
+
     public void CreateLocalPlayer(int id)
     {
         LocalPlayer = new Player(id);
@@ -121,8 +127,9 @@ public class GameManager : MonoBehaviour
     }
 }
 
-
 public enum CardInHand
 {
-    LEFTCARD, MIDDLECARD, RIGHTCARD
+    LEFTCARD,
+    MIDDLECARD,
+    RIGHTCARD
 }
